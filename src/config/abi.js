efbd6edf034d45c042afd5488c6588b8653be9c3
Leonardo Filho <leonardoadsfilho@gmail.com>
2023-12-02 01:27:13 -0300

    1 V

--- src/config/abi.js
+++ src/config/abi.js
@@ -458,7 +458,6 @@ const ElectionContractABI = [
 ]
 
 const ElectionContractAddress = '0x582d80897CDA4e8342323b86448DAF75e5F82AAB'
-const WalletPrivateKey = '0xe3524cc2274fddd4da0a9911f36bad0a518ca6e1f1e706f57442163c7221b401'
 
 const ElectionContract = { 
 	ElectionContractABI, 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { ethers } from 'ethers'
-import WalletContext from '../../Context/Wallet';
+
 import './index.css'
 
 const Home = () => {
 
-    const { wallet } = useContext(WalletContext)
+    
 
     return (
         <div className='vote-page'>
